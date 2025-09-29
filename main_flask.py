ac561768b144e91fb7a0ef00a4dcd38318d9f915
Muhammad Muazzain <90068510+MuhammadMuazzain@users.noreply.github.com>
2025-09-29 20:20:56 +0500

    Main

--- main_flask.py
+++ main_flask.py
@@ -185,3 +185,10 @@ if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
 
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
+
+
